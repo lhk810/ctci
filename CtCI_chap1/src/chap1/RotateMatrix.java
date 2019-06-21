63fd41f5f4278ed7d323961fbda4741b280ef549
이홍기 <@DESKTOP-7TRPVT6>
2019-06-21 22:15:23 +0900

    asd

---
+++ CtCI_chap1/src/chap1/RotateMatrix.java
@@ -0,0 +1,46 @@
+package chap1;
+
+public class RotateMatrix {
+
+	public static void main(String[] args) {
+		int[][] input1 = new int[][] {{1,2,3},{4,5,6},{7,8,9}};
+		int[][] input2 = new int[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+		RotateMatrix RM = new RotateMatrix();
+		RM.printresult(input1);
+		System.out.println();
+		int[][] res1 = RM.rotate(input1);
+		RM.printresult(res1);
+		System.out.println("=========================");
+		RM.printresult(input2);
+		System.out.println();
+		int[][] res2 = RM.rotate(input2);
+		RM.printresult(res2);
+		
+
+	}
+	
+	public int[][] rotate(int[][] input) {
+		int len = input.length-1;
+		for (int i=0; i<input.length/2; i++) {
+			for (int j=0;j<len-2*i;j++) {
+				int tmp = input[j+i][len-i];
+				input[j+i][len-i] = input[i][j+i];
+				input[i][j+i] = input[len-j-i][i];
+				input[len-j-i][i] = input[len-i][len-j-i];
+				input[len-i][len-j-i] = tmp;
+			}
+		}
+		
+		return input;
+	}
+	
+	public void printresult(int[][] input) {
+		for (int i=0; i<input.length; i++) {
+			for (int j=0; j<input.length; j++) {
+				System.out.print(input[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ CtCI_chap1/src/chap1/ZeroMatrix.java
@@ -0,0 +1,71 @@
+package chap1;
+
+public class ZeroMatrix {
+
+	public static void main(String[] args) {
+		int[][] input1 = { {0,1,2} , {1,1,1}, {1,1,0} , {0,1,1}};
+		int[][] input2 = { {0,0,0}, {1,1,1}, {1,1,1}};
+		int[][] input3 = { {1,1,1}, {1,1,1}, {1,1,1}};
+		
+		ZeroMatrix ZM = new ZeroMatrix();
+		ZM.printres(input1);
+		System.out.println();
+		ZM.printres(ZM.makeZero(input1));
+		System.out.println();
+		ZM.printres(input2);
+		System.out.println();
+		ZM.printres(ZM.makeZero(input2));
+		System.out.println();
+		ZM.printres(input3);
+		System.out.println();
+		ZM.printres(ZM.makeZero(input3));
+
+	}
+	
+	public int[][] makeZero(int[][] input) {
+		
+		int m = input.length;
+		int n = input[0].length;
+		
+		int[] row = new int[m];
+		int[] column = new int[n];
+		
+		for (int i=0; i<m; i++) {
+			for (int j=0; j<n; j++) {
+				if (input[i][j]==0) {
+					row[i] = 1;
+					column[j] = 1;
+				}
+			}
+		}
+		
+		for (int i=0; i<m; i++) {
+			if (row[i]==1) {
+				for (int j=0; j<n; j++) {
+					input[i][j] = 0;
+				}
+			}
+		}
+		
+		for (int j=0; j<n; j++) {
+			if (column[j]==1) {
+				for (int i=0; i<m; i++) {
+					input[i][j] = 0;
+				}
+			}
+		}
+		
+		
+		return input;
+	}
+	
+	public void printres(int[][] input) {
+		for (int i=0; i<input.length; i++) {
+			for (int j=0; j<input[0].length; j++) {
+				System.out.print(input[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
