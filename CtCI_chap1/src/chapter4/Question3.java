2d5c729b59f919b1964fee4db0c4e6fa34fd8d0e
Hongki_Lee <Tmax@DESKTOP-1L9FCDK>
2019-07-11 17:52:36 +0900

    Question3 뼈대만

---
+++ CtCI_chap1/src/chapter4/Question3.java
@@ -0,0 +1,18 @@
+package chapter4;
+
+import java.util.ArrayList;
+
+import chapter2.HKLinkedList;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public ArrayList<HKLinkedList> listofdepths(HKTree tree, int level) {
+		return new ArrayList<HKLinkedList>();
+	}
+
+}
