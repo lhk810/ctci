0869c6658d55d7a4c7eb78be78102d46027db32f
이홍기 <@DESKTOP-7TRPVT6>
2019-07-22 21:09:17 +0900

    이거 맞나?

--- CtCI_chap1/src/chapter4/Question9.java
+++ CtCI_chap1/src/chapter4/Question9.java
@@ -6,14 +6,14 @@ public class Question9 {
 
 	public static void main(String[] args) {
 		Question2 q2 = new Question2();
-		int[] arr = new int[3];
+		int[] arr = new int[7];
 		arr[0] = 1;
 		arr[1] = 2;
 		arr[2] = 3;
-		//arr[3] = 7;
-		//arr[4] = 11;
-		//arr[5] = 13;
-		//arr[6] = 15;
+		arr[3] = 7;
+		arr[4] = 11;
+		arr[5] = 13;
+		arr[6] = 15;
 		//arr[7] = 18;
 		//arr[8] = 22;
 		HKTreeNode n = q2.makeTree(arr, 0, arr.length-1);
@@ -48,19 +48,64 @@ public class Question9 {
 	
 	public void makelist(HKTreeNode root, ArrayList<ArrayList<Integer>> list, ArrayList<Integer> sublist) {
 		
-		if (root.left !=null) {
-			ArrayList<Integer> left = copylist(sublist);
-			left.add(root.left.data);
-			list.add(left);
-			makelist(root.left, list, left);
+		if (root.left == null && root.right == null) {
+			return;
 		}
 		
-		if (root.right != null) {
-			ArrayList<Integer> right = copylist(sublist);
-			right.add(root.right.data);
-			list.add(right);
-			makelist(root.right, list, right);
+		/*
+		if (root.right == null && root.left != null) {
+			ArrayList<Integer> left_only = copylist(sublist);
+			left_only.add(root.left.data);
+			list.add(left_only);
+			makelist(root.left, list, left_only);
+		} else if (root.left == null && root.right != null) {
+			ArrayList<Integer> right_only = copylist(sublist);
+			right_only.add(root.right.data);
+			list.add(right_only);
+			makelist(root.right, list, right_only);
+		} else {
+			//add left subtree
+			ArrayList<Integer> left_first = copylist(sublist);
+			left_first.add(root.left.data);
+			makelist(root.left, list, left_first);
+			left_first.add(root.right.data);
+			makelist(root.right, list, left_first);
+			list.add(left_first);
+			
+			//add right subtree
+			ArrayList<Integer> right_first = copylist(sublist);
+			right_first.add(root.right.data);
+			makelist(root.right, list, right_first);
+			right_first.add(root.left.data);
+			makelist(root.left, list, right_first);
+			list.add(right_first);
 		}
+		*/
+		if (root.left != null && root.right != null) {
+			ArrayList<Integer> left_first = copylist(sublist);
+			left_first.add(root.left.data);
+			makelist(root.left, list, left_first);
+			left_first.add(root.right.data);
+			makelist(root.right, list, left_first);
+			list.add(left_first);
+			
+			//add right subtree
+			ArrayList<Integer> right_first = copylist(sublist);
+			right_first.add(root.right.data);
+			makelist(root.right, list, right_first);
+			right_first.add(root.left.data);
+			makelist(root.left, list, right_first);
+			list.add(right_first);
+		} else if (root.left == null && root.right != null) {
+			sublist.add(root.right.data);
+			makelist(root.right, list, sublist);
+			//list.add(sublist);
+		} else if (root.right == null && root.left != null) {
+			sublist.add(root.left.data);
+			makelist(root.right, list, sublist);
+			//list.add(sublist);
+		}
+		
 		
 	}
 	
