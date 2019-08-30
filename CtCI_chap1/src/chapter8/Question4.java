2c38e1b0f78cf3431614be340d7045243a2198d6
hongki_lee <hongki_lee@tmax.co.kr>
2019-08-30 18:31:58 +0900

    Question4 뼈대

---
+++ CtCI_chap1/src/chapter8/Question4.java
@@ -0,0 +1,27 @@
+package chapter8;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public ArrayList<HashSet<Integer>> findSubsets(HashSet<Integer> set, ArrayList<HashSet<Integer>> al, int from, int to) {
+		if (!al.contains(set)) {
+			al.add(set);
+		}
+		
+		HashSet<Integer> newset = new HashSet<>();
+		
+		
+		int target = (from + to)/2; //set은 순서가 유지되지 않는다. 어떻게하냐 이거
+		
+		
+	}
+
+}
