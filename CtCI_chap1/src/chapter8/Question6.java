eb94071ac0fc5216ac1d205a12c2cbe358ffe29f
이홍기 <@DESKTOP-7TRPVT6>
2019-09-05 23:25:46 +0900

    Question6

--- CtCI_chap1/src/chapter8/Question6.java
+++ CtCI_chap1/src/chapter8/Question6.java
@@ -5,8 +5,9 @@ import java.util.Stack;
 public class Question6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Question6 q6 = new Question6();
+		Stack<Integer> result = q6.towerOfHanoi(4);
+		q6.printTower(result);
 	}
 	
 	public Stack<Integer> towerOfHanoi(int num) {
@@ -18,13 +19,31 @@ public class Question6 {
 			stack1.add(i);
 		}
 		
-		
+		moveTower(stack1, stack2, stack3, num);
 		
 		return stack3;
 	}	
 	
-	//n-1만큼을 보조기둥에 옮겨둔다
-	//n크기의 원반을 마지막으로 옮긴다
-	//1번을 보조 기둥으로 n-1만큼을 다시 위 과정 반복한다
+	public void moveTower(Stack<Integer> from, Stack<Integer> middle, Stack<Integer> to, int num) {
+		if (num == 1) {
+			to.add(from.pop());
+			return;
+		} else {
+			moveTower(from, to, middle, num-1);
+			to.add(from.pop());
+			moveTower(middle, from, to, num-1);
+		}
+		
+	}
+	
+	public void printTower(Stack<Integer> stack) {
+		if (stack.size() == 0) {
+			System.out.println("stack is already empty");
+		}
+		
+		while (!stack.empty()) {
+			System.out.println(stack.pop());
+		}
+	}
 
 }
